519bb6f11eafc090b48f464a392bad9643ffcbc4
Karam <Karam.arora20@st.niituniversity.in>
2023-03-30 18:55:06 +0530

    AI_dataset done

--- main.py
+++ main.py
@@ -15,30 +15,32 @@ try:
         i+=1
 except Exception as e:
     pass
+documents=np.array(human_documents+ai_documents)
 
 
+labels = ['human'] * len(human_documents) + ['AI'] * len(ai_documents)
 
-# labels = ['human'] * len(human_documents) + ['AI'] * len(ai_documents)
 
+# vectorizer = TfidfVectorizer(ngram_range=(2,2))
+vectorizer = TfidfVectorizer()
 
-# vectorizer = TfidfVectorizer()
+# Fit the vectorizer to the documents
+vectorizer.fit(documents)
 
-# # Fit the vectorizer to the documents
-# vectorizer.fit(documents)
+# Extract features from the documents
+features = vectorizer.transform(documents)
+# print(np.array(features))
 
-# # Extract features from the documents
-# features = vectorizer.transform(documents)
+# Split the feature matrix and labels into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features.toarray(), labels, test_size=0.2)
+print(X_train)
+# Train a Random Forest classifier on the training data
+rf_classifier = RandomForestClassifier()
+rf_classifier.fit(X_train, y_train)
 
-# # Split the feature matrix and labels into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(features.toarray(), labels, test_size=0.2)
+# Test the classifier on the testing data
+predictions = rf_classifier.predict(X_test)
 
-# # Train a Random Forest classifier on the training data
-# rf_classifier = RandomForestClassifier()
-# rf_classifier.fit(X_train, y_train)
-
-# # Test the classifier on the testing data
-# predictions = rf_classifier.predict(X_test)
-
-# # Print the accuracy of the classifier
-# accuracy = sum(predictions == y_test) / len(y_test)
-# print("Accuracy:", accuracy)
+# Print the accuracy of the classifier
+accuracy = sum(predictions == y_test) / len(y_test)
+print("Accuracy:", accuracy)
